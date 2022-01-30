8edc6eaba1a9f65de264bd8e14b77226efbba852
Yaroslav Dyshkantiuk <yaroslav.dyshkantiuk@gmail.com>
2022-01-30 22:07:24 +0200

    handling forms

--- src/components/todo-list-item/todo-list-item.js
+++ src/components/todo-list-item/todo-list-item.js
@@ -4,30 +4,12 @@ import './todo-list-item.css';
 
 export default class TodoListItem extends Component {
 
-  state = {
-    done: false,
-    important: false
-  };
-
-  onLabelClick = () => {
-    this.setState(({done}) => {
-      return {
-        done: !done
-      };
-    });
-  };
-
-  onMarkImportant = () => {
-    this.setState(({important}) => {
-      return {
-        important: !important
-      };
-    });
-  };
-
   render() {
-    const { label, onDeleted } = this.props;
-    const { done, important } = this.state;
+
+    const { label, onDeleted,
+            onToggleImportant,
+            onToggleDone,
+            important, done } = this.props;
 
     let classNames = 'todo-list-item';
     if (done) {
@@ -42,13 +24,13 @@ export default class TodoListItem extends Component {
       <span className={classNames}>
         <span
           className="todo-list-item-label"
-          onClick={ this.onLabelClick }>
+          onClick={onToggleDone}>
           {label}
         </span>
 
         <button type="button"
                 className="btn btn-outline-success btn-sm float-right"
-                onClick={this.onMarkImportant}>
+                onClick={onToggleImportant}>
           <i className="fa fa-exclamation" />
         </button>
 
